6b9ddae121d08f003c47db0157ec1c88356cfc8b
Priyanka-191925 <sainipriyanka1722001@gmail.com>
2023-05-08 11:38:10 +0530

    javascript concepts

---
+++ function.js
@@ -0,0 +1,17 @@
+console.log("This is in CSS file.")
+
+function comparision(num1,num2){
+    if (num1>num2){
+        console.log(num1+ "is greater than "+num2);
+    }
+    else{
+        console.log(num2+ " is greater than "+num1);
+        }
+    let returnval=1;
+    return returnval;
+}
+
+num1 =4;
+num2 = 5;
+re =comparision(num1,num2);
+console.log(re)
