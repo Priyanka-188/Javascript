aafb4d48a60c11bd64d23fe3194981173ff5c7eb
Priyanka <sainipriyanka1722001@gmail.com>
2023-12-09 10:42:18 +0530

    hoisting in java and negative inifinity

---
+++ hoisting.js
@@ -0,0 +1,36 @@
+hoistedVariable = "Kridha";
+// console.log(hoistedVariable);
+var hoistedVariable ;
+
+// hoisted();
+
+function hoisted(){
+    console.log("I am a hoisted function");
+}
+
+var hoisted1 = function(){
+    console.log("I am an expression function using function operator ");
+}
+// hoisted1();
+
+
+
+//Hoisting in local scope
+// doSomething();
+
+function doSomething(){
+    x=11;
+    console.log(x);
+    var x;
+}
+// doSomething();
+
+
+
+//variable declared using var can be initalised before declaration but in case of let ,it throws an error. 
+
+var num = -5;
+function negativeInfinity(){
+    console.log(num/0);
+}
+negativeInfinity();
